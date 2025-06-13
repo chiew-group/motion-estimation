3b2b3a522e9507100bece8390c50ff6704641944
Giuseppe Grossi <giuseppe.volpe.grossi@gmail.com>
2025-06-13 16:48:34 -0400

    Added a loop for nm iterations

--- joint_recon.py
+++ joint_recon.py
@@ -33,11 +33,13 @@ class JointRecon:
         self.img_shape = ksp.shape[1:]
 
         #Data needs to be uncentered and then moved onto the device
-        self.ksp = cp.fft.fftshift(cp.array(ksp))
-        self.mps = cp.fft.fftshift(cp.array(mps))
-        self.shot_mask = cp.fft.fftshift(cp.array(shot_mask))
-    
-        self.x = cp.zeros(self.img_shape, dtype=ksp.dtype)
+        self.ksp = cp.fft.ifftshift(cp.array(ksp),axes=(-3,-2,-1)).astype(cp.complex64)
+        self.mps = cp.fft.ifftshift(cp.array(mps), axes=(-3,-2,-1))
+        self.shot_mask = cp.fft.ifftshift(cp.array(shot_mask), axes=(-3,-2,-1))
+
+        self.max_norm = cp.max(cp.abs(cp.fft.ifftn(self.ksp, axes=(-3,-2,-1), norm='ortho')))
+        self.ksp /= self.max_norm
+        self.x = cp.zeros(self.img_shape, dtype=self.ksp.dtype)
         if t0 is None:
             self.t = cp.zeros((self.num_shots,6), dtype=float)
         else:
@@ -51,31 +53,36 @@ class JointRecon:
 
         self.damp = xp.ones((self.num_shots))
         self.convergence_flags = xp.zeros((self.num_shots), dtype=bool)
-
+        #self.P = 1 / (cp.sum(cp.abs(self.mps) ** 2, axis=0) + 1e-6)
+        self.P = cp.ones(self.img_shape)
+        #self.M = (cp.sum(cp.abs(self.mps) ** 2, axis=0) > 0.1)
+        self.M = cp.ones(self.img_shape)
 
     def run(self):
         self.transform_history = []
         self.objective_history = []
 
         for iter in tqdm(range(self.max_joint_iter)):
-            self.x = estimate_image_cg(self.ksp, self.mps, self.shot_mask, self.t, self.kgrid, self.rkgrid, 
+            self.x = estimate_image_cg(self.ksp, self.mps, self.shot_mask, self.t, self.kgrid, self.rkgrid, self.P, self.M,
                                     x0=self.x, max_iter=self.max_cg_iter)
-            estimate_transform(self.ksp, self.mps, self.shot_mask, self.x, self.kgrid, self.rkgrid,
-                                        self.damp, self.convergence_flags,
-                                        self.residual_buf, self.partial_buf, self.gradient_buf, self.hessian_buf,
-                                        transforms=self.t, max_iter=self.max_nm_iter)
+            for _ in range(self.max_nm_iter):
+                self.x = estimate_transform(self.ksp, self.mps, self.shot_mask, self.x, self.kgrid, self.rkgrid,
+                                            self.damp, self.convergence_flags,
+                                            self.residual_buf, self.partial_buf, self.gradient_buf, self.hessian_buf,
+                                            transforms=self.t, max_iter=self.max_nm_iter)
+                self.x *= self.M
             self.transform_history.append(cp.asnumpy(self.t.copy()))
             self.objective_history.append(self._compute_objective())
 
         self.transform_history = np.stack(self.transform_history)
         self.objective_history = np.array(self.objective_history)
-        return cp.fft.fftshift(self.x), self.t
+        return cp.fft.fftshift(self.x * self.max_norm), self.t
             
     def _compute_objective(self):
         total_loss = 0.0
         for s in range(self.num_shots):
             T = RigidTransformCudaOptimzied(self.t[s], self.kgrid, self.rkgrid)
-            Ex = cp.fft.fftn(self.mps * T.apply(self.x), axes=(-3,-2,-1))
+            Ex = cp.fft.fftn(self.mps * T.apply(self.x), axes=(-3,-2,-1), norm='ortho')
             resid = self.shot_mask[s] * (Ex - self.ksp)
-            total_loss += (cp.vdot(resid, resid).real / resid.size)
+            total_loss += cp.vdot(resid, resid).real
         return total_loss.item()
